19eb752cbb5bf47b7fde3abe11b9b85755f3dc91
Naman <102941774+Naman0918@users.noreply.github.com>
2023-10-25 23:44:50 +0530

    Update FIFO.c

--- Assignment - 6/FIFO.c
+++ Assignment - 6/FIFO.c
@@ -1,84 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-using namespace std;
-int main(){
-    int n,m,i,j,k,hit=0;
-    cout<<"Enter number of frames\n";
-    cin>>n;
-    cout<<"Enter number of processes\n";
-    cin>>m;
-    vector<int> p(m);
-    vector<int> hi(m);
-    cout<<"Enter processes\n";
-    for(i=0;i<m;i++){
-        cin>>p[i];
+int main() {
+    int n, m, i, j, k, hit = 0;
+    printf("Enter number of frames\n");
+    scanf("%d", &n);
+    printf("Enter number of processes\n");
+    scanf("%d", &m);
+
+    int p[m];
+    int hi[m];
+    printf("Enter processes\n");
+    for (i = 0; i < m; i++) {
+        scanf("%d", &p[i]);
     }
-    vector<vector<int>> a(n);
-    for(i=0;i<n;i++){
-        a[i]=vector<int>(m,-1);
-    }
-    map <int, int> mp;    
-    for(i=0;i<m;i++){
-        vector<pair<int,int>> c;
-        for(auto q: mp){
-            c.push_back({q.second,q.first});
+
+    int a[n][m];
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            a[i][j] = -1;
         }
-        sort(c.begin(),c.end());
-        bool hasrun=false;
-        for(j=0;j<n;j++){
-            if(a[j][i]==p[i]){
+    }
+
+    int mp[m];
+    for (i = 0; i < m; i++) {
+        mp[i] = 0;
+    }
+
+    for (i = 0; i < m; i++) {
+        int c[m][2];
+        int idx = 0;
+        for (int q = 0; q < m; q++) {
+            c[idx][0] = mp[q];
+            c[idx][1] = q;
+            idx++;
+        }
+
+        for (int p1 = 0; p1 < idx; p1++) {
+            for (int p2 = p1 + 1; p2 < idx; p2++) {
+                if (c[p1][0] > c[p2][0]) {
+                    int temp0 = c[p1][0];
+                    int temp1 = c[p1][1];
+                    c[p1][0] = c[p2][0];
+                    c[p1][1] = c[p2][1];
+                    c[p2][0] = temp0;
+                    c[p2][1] = temp1;
+                }
+            }
+        }
+
+        int hasrun = 0;
+        for (j = 0; j < n; j++) {
+            if (a[j][i] == p[i]) {
                 hit++;
-                hi[i]=1;
+                hi[i] = 1;
                 mp[p[i]]++;
-                hasrun=true;
+                hasrun = 1;
                 break;
             }
-            if(a[j][i]==-1){
-                for(k=i;k<m;k++)
-                    a[j][k]=p[i];
+            if (a[j][i] == -1) {
+                for (k = i; k < m; k++)
+                    a[j][k] = p[i];
                 mp[p[i]]++;
-                hasrun=true;
+                hasrun = 1;
                 break;
             }
         }
-        if(j==n||hasrun==false){
-            for(j=0;j<n;j++){
-                if(a[j][i]==c[c.size()-1].second){
-                    mp.erase(a[j][i]);
-                    for(k=i;k<m;k++)
-                        a[j][k]=p[i];
+
+        if (j == n || hasrun == 0) {
+            for (j = 0; j < n; j++) {
+                if (a[j][i] == c[m - 1][1]) {
+                    mp[a[j][i]] = 0;
+                    for (k = i; k < m; k++)
+                        a[j][k] = p[i];
                     mp[p[i]]++;
                     break;
                 }
             }
         }
-        for(auto q:mp){
-            if(q.first!=p[i]){
-                mp[q.first]++;
+
+        for (int q = 0; q < m; q++) {
+            if (q != p[i]) {
+                mp[q]++;
             }
         }
     }
-    cout<<"Process ";
-    for(i=0;i<m;i++){
-        cout<<p[i]<<" ";
+
+    printf("Process ");
+    for (i = 0; i < m; i++) {
+        printf("%d ", p[i]);
     }
-    cout<<'\n';
-    for(i=0;i<n;i++){
-        cout<<"Frame "<<i<<" ";
-        for(j=0;j<m;j++){
-            if(a[i][j]==-1)
-                cout<<"E ";
-                else 
-            cout<<a[i][j]<<" ";
+    printf("\n");
+
+    for (i = 0; i < n; i++) {
+        printf("Frame %d ", i);
+        for (j = 0; j < m; j++) {
+            if (a[i][j] == -1)
+                printf("E ");
+            else
+                printf("%d ", a[i][j]);
         }
-        cout<<'\n';
+        printf("\n");
     }
-    for(i=0;i<m;i++){
-        if(hi[i]==0)
-        cout<<"  ";
+
+    for (i = 0; i < m; i++) {
+        if (hi[i] == 0)
+            printf("  ");
         else
-        cout<<hi[i]<<" ";
+            printf("%d ", hi[i]);
     }
-    cout<<"\n";
-    cout<<"Hit "<<hit<<'\n'<<"Page Fault "<<m-hit<<'\n';
+    printf("\n");
+
+    printf("Hit %d\nPage Fault %d\n", hit, m - hit);
     return 0;
 }
