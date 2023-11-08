d6bc4a6002e6366a636192e706909acf8f0342d0
Naman <102941774+Naman0918@users.noreply.github.com>
2023-11-08 20:36:15 +0530

    Update Combine.c

--- Assignment - 6/Combine.c
+++ Assignment - 6/Combine.c
@@ -1,311 +1,234 @@
 #include <stdio.h>
-int miss = 0, hit = 0;
-int n, f;
-int arr[100][100];
+#include <stdlib.h>
+
+int hit = 0;
+int pagefault = 0;
 int p[100];
-int frame[100];
+int frame[20];
+int n;
+int nf;
 
-int s = 0;
-
-void display()
+void getprocess()
 {
-    int x = 1;
-    for (int i = 0; i < f; i++)
+    printf("Enter number of process : ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
     {
-        printf("f%d ", x);
-        x++;
-        for (int j = 0; j < n; j++)
-        {
-            if (arr[i][j] == -1)
-            {
-                printf("*  ");
-            }
-            else
-            {
-                printf("%d  ", arr[i][j]);
-            }
-        }
-        printf("\n");
+        printf("Enter process number : ");
+        scanf("%d", &p[i]);
     }
-
-    printf("Hit %d\n", hit);
-    printf("Miss %d\n", miss);
+    printf("Enter frame size : ");
+    scanf("%d", &nf);
 }
+
+void init()
+{
+    int hit = 0;
+    int pagefault = 0;
+    for (int i = 0; i < nf; i++)
+    {
+        frame[i] = 9999;
+    }
+}
+
+void pagedisplay()
+{
+    for (int i = 0; i < nf; i++)
+    {
+        if (frame[i] != 9999)
+        {
+            printf("%d ", frame[i]);
+        }
+    }
+}
+
+int ishit(int page) // return index if hit else return -1
+{
+    // explain function
+    // if hit return index of page in frame
+    int ishit = -1;
+    for (int i = 0; i < nf; i++)
+    {
+        if (frame[i] == page)
+        {
+            ishit = i;
+            hit++;
+        }
+    }
+    return ishit;
+}
+
 void fifo()
 {
-    for (int i = 0; i < f; i++)
-    {
-        frame[i] = -1;
-    }
+    init(); // initialize frame
+    printf("----------------------fifo--------------------\n");
+
     for (int i = 0; i < n; i++)
     {
-        int cnt = 0;
+        printf("\nfor process id -> %d", p[i]); // p[i] is page in process array
         int j = 0;
-        for (; j < f; j++)
+        if (ishit(p[i]) == -1)
         {
-            if (frame[j] == -1)
+            printf(" Miss : ");
+            for (j = 0; j < nf - 1; j++) // explain for loop and j<nf-1 condition
+            // j<nf-1 because we are shifting frame to left by 1 position and we are inserting new page at last position
             {
-                cnt++;
-                break;
+                frame[j] = frame[j + 1]; // shift frame to left by 1 position to make space for new page
             }
-        }
-        if (cnt != 0)
-        {
-            miss++;
-            frame[j] = p[i];
+            frame[j] = p[i]; // insert new page at last position
+            // what is value of j here if nf = 3
+            // j=2
+            pagefault++;   // increment pagefault
+            pagedisplay(); // display frame
         }
         else
         {
-            int c = 0;
-
-            for (int d = 0; d < f; d++)
-            {
-                if (frame[d] == p[i])
-                {
-                    c++;
-                    break;
-                }
-            }
-            if (c == 0)
-            {
-                miss++;
-                frame[s] = p[i];
-                s += 1 % f;
-            }
-            else
-            {
-                hit++;
-            }
-        }
-        for (int k = 0; k < f; k++)
-        {
-            arr[k][i] = frame[k];
+            printf(" Hit  : ");
+            pagedisplay();
         }
     }
-    display();
+    printf("\n");
+    printf("\nTotal No.of page fault are :%d", pagefault);
+    printf("\nTotal No.of hits are :%d", (n - pagefault));
 }
-void lru()
+
+void lru() // full form of lru - least recently used
 {
-    for (int i = 0; i < f; i++)
-    {
-        frame[i] = -1;
-    }
+    init();
+    printf("------------------------LRU-----------------------\n");
+    int j = 0;
     for (int i = 0; i < n; i++)
     {
-        int cnt = 0;
-        int j = 0;
-        for (; j < f; j++)
+        printf("\nfor process id -> %d", p[i]);
+        int key = ishit(p[i]);
+        if (ishit(p[i]) == -1)
         {
-            if (frame[j] == -1)
+            printf(" Miss : ");
+            for (j = 0; j < nf - 1; j++)
             {
-                cnt++;
-                break;
+                frame[j] = frame[j + 1];
             }
-        }
-        if (cnt != 0)
-        {
-            miss++;
             frame[j] = p[i];
+            pagedisplay();
+            pagefault++;
         }
         else
         {
-            int count = 0;
-            for (int k = 0; k < f; k++)
+            printf(" Hit  : ");
+            for (j = key; j < nf - 1; j++) // explain for loop and j<nf-1 condition
+            // j<nf-1 because we are shifting frame to left by 1 position and we are inserting new page at last position
+            // j=key because we are shifting frame to left by 1 position and we are inserting new page at last position
+            // key is index of page in frame
+            // if key=1 and nf=3
+            // if key = 4 then j=4 and j<nf-1 is false so loop will not execute
             {
-                // Checking hit condition
-                if (p[i] == frame[k])
-                {
-                    count++;
-                    break;
-                }
+                frame[j] = frame[j + 1];
             }
-            if (count != 0)
-            {
-                // If hit then count of hit is added
-                hit++;
-            }
-            else
-            {
-                miss++;
-                int fReplace[f];
-
-                for (int j = 0; j < f; j++)
-                {
-                    fReplace[j] = -1;
-                    /*storing the indexes of the frame elements to find the farthest element*/
-                    for (int k = i - 1; k >= 0; k--)
-                    {
-                        if (p[k] == frame[j])
-                        {
-                            fReplace[j] = k;
-                            break;
-                        }
-                    }
-                }
-                // Finding the farthest frame
-
-                int miniIndex = 1e9;
-                int ind = -1;
-                for (int j = 0; j < f; j++)
-                {
-                    if (fReplace[j] == -1)
-                    {
-                        ind = j;
-                        break;
-                    }
-                    if (miniIndex > fReplace[j])
-                    {
-                        miniIndex = fReplace[j];
-                        ind = j;
-                    }
-                }
-                // frame replaced
-                // printf("%d-F\n",fReplace);
-                frame[ind] = p[i];
-            }
-        }
-
-        for (int j = 0; j < f; j++)
-        {
-            arr[j][i] = frame[j];
+            frame[j] = p[i];
+            pagedisplay();
         }
     }
-    display();
+    printf("\n");
+    printf("\nTotal No.of page fault are :%d", pagefault);
+    printf("\nTotal No.of hits are :%d", (n - pagefault));
 }
+
 void optimal()
 {
-    for (int i = 0; i < f; i++)
-    {
-        frame[i] = -1;
-    }
+    init();
+    printf("-----------------optimal-----------------\n");
+    int opt[n];
 
-    for (int i = 0; i < n; i++)
+    int i, j, k;
+    for (i = 0; i < n; i++)
     {
-        int cnt = 0;
-        int j = 0;
-        for (; j < f; j++)
+        if (ishit(p[i]) == -1)
         {
-            if (frame[j] == -1)
+            printf(" Miss : ");
+            for (j = 0; j < nf; j++)
             {
-                cnt++;
-                break;
+                int pg = frame[j]; // pg is page in frame meaning page in frame at index j
+                // pg is page in frame
+                int found = 0;
+                for (k = i + 1; k < n; k++) // IMPORTANT RANGE
+                {
+                    if (p[k] == pg)
+                    {
+                        opt[j] = k; // why j is used here
+                        // j is index of page in frame
+                        // opt is array which stores index of page in process array
+                        found = 1;
+                        // if page is found in process array then break loop
+                    }
+                }
+                if (!found)
+                {
+                    opt[j] = 99999;
+                }
             }
-        }
-        if (cnt != 0)
-        {
-            miss++;
-            frame[j] = p[i];
+
+            int max = -99999;
+            int maxind;
+            for (int i = 0; i < nf; i++)
+            // explain for loop
+            // we are finding page which is not used for longest time
+            {
+                if (opt[i] > max) // opt[i] is index of page in process array
+                {
+                    max = opt[i];
+                    maxind = i;
+                }
+            }
+            // explain maxind
+            // maxind is index of page in frame which is not used for longest time
+            // if maxind=1 then page at index 1 in frame is not used for longest time
+            frame[maxind] = p[i]; // insert new page at index maxind in frame
+            pagedisplay();
+            pagefault++;
         }
         else
         {
-            int c = 0;
-
-            for (int j = 0; j < f; j++)
-            {
-                if (frame[j] == p[i])
-                {
-                    c++;
-                    break;
-                }
-            }
-            if (c != 0)
-            {
-                hit++;
-            }
-            else
-            {
-                miss++;
-                int l = 0;
-                int fr[f];
-
-                int cn = 0;
-                for (int k = 0; k < f; k++)
-                {
-                    fr[k] = -1;
-                    for (int j = i + 1; j < n; j++)
-                    {
-                        if (p[j] == frame[k])
-                        {
-                            fr[k] = j;
-                            cn++;
-                            break;
-                        }
-                    }
-                }
-
-                int mx = -1;
-                int ind = 0;
-                for (int l = 0; l < f; l++)
-                {
-                    if (fr[l] == -1)
-                    {
-
-                        ind = l;
-                        break;
-                    }
-                    if (fr[l] > mx)
-                    {
-                        mx = fr[l];
-                        ind = l;
-                    }
-                }
-                frame[ind] = p[i];
-            }
-        }
-        for (int k = 0; k < f; k++)
-        {
-            arr[k][i] = frame[k];
+            printf(" Hit  : ");
+            pagedisplay();
         }
     }
-    display();
+    printf("\n");
+    printf("\nTotal No.of page fault are :%d", pagefault);
+    printf("\nTotal No.of hits are :%d", (n - pagefault));
 }
 
 int main()
 {
-    printf("Enter the size of the reference String :: ");
-    scanf("%d", &n);
-    printf("Enter the reference string :: ");
+    int choice;
+    
 
-    for (int i = 0; i < n; i++)
+    while (choice != 4)
     {
-        scanf("%d", &p[i]);
-    }
-    printf("Enter the frame size :: ");
-    scanf("%d", &f);
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < f; j++)
-        {
-            arr[j][i] = -1;
-        }
-    }
-    int flag = 1;
-    while (flag)
-    {
-        printf("Menu :: 1) FIFO\t2) LRU\t3) Optimal  4) Exit\nChoice :: ");
-        int ch;
-        scanf("%d", &ch);
-        printf("\n");
-        switch (ch)
+        printf("**********MENU**********\n");
+        printf("1.FIFO\n2.LRU\n3.Optimal\n4.EXIT\n");
+        printf("************************\n");
+        printf("Enter your choice : ");
+        scanf("%d", &choice);
+        getprocess();
+        switch (choice)
         {
         case 1:
             fifo();
             break;
+            printf("\n");
         case 2:
-            // LRU();
             lru();
             break;
+            printf("\n");
         case 3:
             optimal();
             break;
-        case 4:
-            flag = 0;
-            printf("\nExiting ...\n");
-            break;
+            printf("\n");
         default:
-            printf("Invalid Input !!!\n\n");
+            printf("Invalid choice");
             break;
         }
     }
+
+    return 0;
 }
